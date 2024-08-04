997f3a45f03b1a01d5c70eb02f93c66c820c0a8c
krish4uu <prajapatikashyap97@gmail.com>
2024-08-03 20:38:51 -0400

    Updated dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    images: {
+      unoptimized: true,
+      loader: "akamai",
+      path: "/",
+    },
+};
 export default nextConfig;
